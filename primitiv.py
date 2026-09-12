fafb95d4da7e28f930d9479c04b76a5164733f91
Kyle <abdulaxadjon9@gmail.com>
2026-09-13 07:34:48 +0900

    feat:learn string in python

--- primitiv.py
+++ primitiv.py
@@ -1,13 +1,40 @@
-print("======NUMBER======")
+# print("======NUMBER======")
 
 # in Java and C , variable is a name of storage location!
 # in Python , VAriable is named reference!
 
-count = 100
+# count = 100
 # count_type = type(count)
 # print("count:", count_type)
 # print(f"the count:{count} and type:{count_type}")
 
-result1 = count.bit_count()  # method
-result2 = count.numerator  # state
-print(f"the result1:{result1} and result2:{result2}")
+# result1 = count.bit_count()  # method
+# result2 = count.numerator  # state
+# print(f"the result1:{result1} and result2:{result2}")
+
+
+# print("======string======")
+# # Method
+
+# '''
+# upper(),lower(),title(),find(),replace()
+# '''
+
+
+# course = "AI Python FullStack"
+
+# result = (type(course))
+# print(f"result (1):{result}")
+
+# result = course.title()
+# print(f"result (2):{result}")
+
+
+# result = course.upper()
+# print(f"result (3):{result}")
+
+# result = course.replace("FullStack", "MasterClass")
+# print(f"result (4):{result}")
+
+
+print("======boolean======")

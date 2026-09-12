02093bd07a49b0726ea51ae9fe0404063725616e
Kyle <abdulaxadjon9@gmail.com>
2026-09-13 06:22:50 +0900

    feat:learn numbers in python

---
+++ primitiv.py
@@ -0,0 +1,13 @@
+print("======NUMBER======")
+
+# in Java and C , variable is a name of storage location!
+# in Python , VAriable is named reference!
+
+count = 100
+# count_type = type(count)
+# print("count:", count_type)
+# print(f"the count:{count} and type:{count_type}")
+
+result1 = count.bit_count()  # method
+result2 = count.numerator  # state
+print(f"the result1:{result1} and result2:{result2}")

bf362e27fb021fce72c06b71739bc525ea1a1f19
Kyle <abdulaxadjon9@gmail.com>
2026-09-13 06:14:31 +0900

    feat: oython system variables and specila methods via dunders

--- run.py
+++ run.py
@@ -1,5 +1,17 @@
+# Dunder  _builtins_ __init__
+
 a = 147
 print("a:", a)
 message = "Hello, World!"
 print("message:", message)
 print("This is a sample Python script.")
+
+
+'''
+In python , there are builtin tools:
+      Types>int,float,str,list,dict
+      Functions>print(),len(),input(),type()
+      Constants>True,False,None
+
+'''
+print(dir(__builtins__))

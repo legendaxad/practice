da6e0e957d581372f2ff7da0f9404234776f1a8f
Kyle <abdulaxadjon9@gmail.com>
2026-09-13 09:38:36 +0900

    feat:learn keyboard and defoult arguments

--- function.py
+++ function.py
@@ -6,24 +6,43 @@
       (4) Scope
 '''
 
-print("=====DEFINE(parameter) & CALL(argumeter) =====")
+# print("=====DEFINE(parameter) & CALL(argumeter) =====")
 # funtion> reusable blovk of code
 # Instead of block {} in Java but in Python uses indentation!
 
 # Define - parameter
 
 
-def greet(a):
-    print(f"how do you do? {a}")
+# def greet(a):
+#     print(f"how do you do? {a}")
 
 
-def greeting(b):
-    print(f"greeting is executed")
-    return f"Hi {b}"
+# def greeting(b):
+#     print(f"greeting is executed")
+#     return f"Hi {b}"
 
 
-# CALL - argument
-result1 = greet("Kyle")
-result2 = greeting("Abbos")
-print(result1)
-print(result2)
+# # CALL - argument
+# result1 = greet("Kyle")
+# result2 = greeting("Abbos")
+# print(result1)
+# print(result2)
+
+
+# print("===== Keyword vs default arguments =====")
+
+# # Define
+
+
+# def give_greet(name, age=11):
+#     print("give_greet is executed")
+#     return f"Hi {name} , you are {age} years old"
+
+
+# # CALL
+
+# result3 = give_greet(name="Kyle", age=22)
+# print("result3:", result3)
+
+# result4 = give_greet(name="John")
+# print("result4:", result4)

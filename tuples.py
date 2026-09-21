b4ad4a1092f516e95429e412a1159008aa878820
Kyle <abdulaxadjon9@gmail.com>
2026-09-21 14:02:07 +0900

    feat:TUple and list

---
+++ tuples.py
@@ -0,0 +1,29 @@
+'''
+      Tuple
+      (1) what is tiple : tuple vs list
+      (2) unpacking argiments
+      (3) zip
+'''
+
+print("===== Whta is tuple : tuple vs list =====")
+
+# Java/php/nodejs array => Python list,array
+
+# list
+# numbs = [1, 4, 2, 6, 3, 3]
+# car_dic = {"brand": "Ferrari", "year": "2002"}
+# print(numbs)
+# # Contructor
+# letters = list("Hello World!")
+# person_dic = dict(name="Kyle", age=22)
+# print(letters)
+
+fruits = ["apple", "lemon", "banana", "melon"]
+print(fruits)
+fruits[0] = "kiwi"
+print(fruits)
+
+
+animal = ("dog", "cat", "fish", "lion")
+tuple_obj = ("Mit", 100, True, None)
+print(animal[2])
